f3fcfa90d702c31e24ded875d33b73c3373a1d5d
mateusf-prog <mateus102006@hotmail.com>
2023-07-30 11:00:17 -0300

    add and using custom properties in application using @Value

--- src/main/java/br/com/mateus/demo/rest/FunRestController.java
+++ src/main/java/br/com/mateus/demo/rest/FunRestController.java
@@ -1,10 +1,18 @@
 package br.com.mateus.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
 
     @GetMapping("/")
     public String sayHello() {
@@ -20,4 +28,10 @@ public class FunRestController {
     public String fortunePage() {
         return "5 billion dollars!";
     }
+
+    @GetMapping("/teaminfo")
+    public String getCoachName() {
+        return "Coach: " + coachName + ", team name: " + teamName;
+    }
+
 }
